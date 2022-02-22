c00f10078b3ecf1a24c2d1f3fa3a00016a838c21
sithi5 <ma.sithis@gmail.com>
2022-02-22 18:31:34 +0100

    [wip]: Add watchlist

--- screens/index.tsx
+++ screens/index.tsx
@@ -2,7 +2,7 @@ import LibraryScreen from './LibraryScreen';
 import ItemDetailsScreen from './ItemDetailsScreen';
 import SearchAnimeScreen from './SearchAnimeScreen';
 import SearchMangaScreen from './SearchMangaScreen';
-import WatchListScreen from './WatchlistScreen';
+import WatchListScreen from './WatchListScreen';
 
 export {
     LibraryScreen,
