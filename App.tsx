a88d7b8d886d1c209e3135b8f6509adbf6406cdb
sithi5 <ma.sithis@gmail.com>
2022-03-15 20:27:27 +0100

    [wip]: try to fix standalone
     status bar pb

--- App.tsx
+++ App.tsx
@@ -1,19 +1,27 @@
 import { NavigationContainer } from '@react-navigation/native';
 import 'config/firebase';
+import AppLoading from 'expo-app-loading';
+import { useFonts } from 'expo-font';
 import React from 'react';
 // Ignore annoying warning comming from firebase
-import { LogBox } from 'react-native';
+import { LogBox, Platform, StatusBar } from 'react-native';
 import 'react-native-gesture-handler'; //⚠️ Keep at top of the file ⚠️
 import { Provider } from 'react-redux';
 import RootBottomTabNavigator from './navigations/RootNavigations';
 import { store } from './redux/Store';
-import AppLoading from 'expo-app-loading';
-import { useFonts } from 'expo-font';
 
 LogBox.ignoreLogs([
     "AsyncStorage has been extracted from react-native core and will be removed in a future release. It can now be installed and imported from '@react-native-async-storage/async-storage' instead of 'react-native'. See https://github.com/react-native-async-storage/async-storage",
 ]);
 LogBox.ignoreLogs(['Setting a timer']);
+
+// Fix status bar bug with standalone app.
+StatusBar.setBarStyle('dark-content');
+
+if (Platform.OS === 'android') {
+    StatusBar.setTranslucent(true);
+    StatusBar.setBackgroundColor('transparent');
+}
 
 export default function App() {
     let [fontsLoaded] = useFonts({
